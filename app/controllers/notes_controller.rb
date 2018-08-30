580f8d047511349dcfbce4d7ed1ba3a0c8894c45
Nichol Blackburn <nicholblackburn@outlook.com>
2018-08-30 16:01:43 -0600

    updated controllers and views

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,32 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create 
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def note_params
+    params.require(:note).permit( :title, :body)
+  end
+
+end
+
+ 
